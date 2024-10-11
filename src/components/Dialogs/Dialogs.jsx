d2ee373d74b79bd1633f4f6ca768bece60900b6c
kt-olya <kt.olya@mail.ru>
2024-10-11 16:12:28 +0300

    21_dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,21 @@
 import s from "./Dialogs.module.css";
 const Dialogs = (props) => {
-  return <div class="s.content">Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Valera</div>
+        <div className={s.dialog + " " + s.active}>Max</div>
+        <div className={s.dialog}>Vlad</div>
+        <div className={s.dialog}>Dima</div>
+        <div className={s.dialog}>Lesha</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>HI</div>
+        <div className={s.message}>How are you</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  );
 };
+
 export default Dialogs;
