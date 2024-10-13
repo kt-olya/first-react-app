c929403eaebe7e3793a53724a3a76a3608baf670
kt-olya <kt.olya@mail.ru>
2024-10-13 08:02:37 +0300

    22_dialogs_props

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,10 +4,18 @@ const Dialogs = (props) => {
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
         <div className={s.dialog}>Valera</div>
-        <div className={s.dialog + " " + s.active}>Max</div>
-        <div className={s.dialog}>Vlad</div>
-        <div className={s.dialog}>Dima</div>
-        <div className={s.dialog}>Lesha</div>
+        <div className={s.dialog + " " + s.active}>
+          <NavLink to="/dialogs/1">Max</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2">Vlad</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/3"></NavLink>Dima
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4">Lesha</NavLink>
+        </div>
       </div>
       <div className={s.messages}>
         <div className={s.message}>HI</div>
